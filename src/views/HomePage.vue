045a911a95dac838f61eeaa184b43c26fed6d2fb
Armando233-abc <armandovigliotti5@gmail.com>
2022-10-24 15:29:35 +0200

    basic css style for mobile

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,16 +7,20 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-input placeholder="Enter company name" id="input_field"></ion-input>
-      <ion-button v-on:click="getInputValue()">"Salva"</ion-button>
-      <ion-list>
-        <ion-item v-for="i in input_list" :key="i">
-          <ion-label>
-            {{ i }}
-            <ion-button v-on:click="elimina(i)">Elimina</ion-button>
-          </ion-label>
-        </ion-item>
-      </ion-list>
+      <ion-item id="input_container">
+        <ion-input placeholder="Enter company name" id="input_field"></ion-input>
+        <ion-button v-on:click="getInputValue()">"Salva"</ion-button>
+      </ion-item>
+      <ion-item id="list_container">
+        <ion-list>
+          <ion-item v-for="i in input_list" :key="i">
+            <ion-label id="component">
+              <h1>{{ i }}</h1>
+              <ion-button v-on:click="elimina(i)">Elimina</ion-button>
+            </ion-label>
+          </ion-item>
+        </ion-list>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
@@ -41,7 +45,7 @@ export default defineComponent({
   },
   data() {
     return {
-      input_list: []
+      input_list: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
     }
   },
   methods: {
@@ -53,14 +57,14 @@ export default defineComponent({
     elimina: function (el) {
       const index = this.input_list.indexOf(el)
       const length = this.input_list.length
-      if (index === 0){
+      if (index === 0) {
         this.input_list.shift()
-      } else if (index === length - 1){
+      } else if (index === length - 1) {
         this.input_list.pop()
-      } else{
+      } else {
         const new_input_list = []
-        for(let i = 0; i < length; i++){
-          if(i !== index){
+        for (let i = 0; i < length; i++) {
+          if (i !== index) {
             new_input_list.push(this.input_list[i])
           }
         }
@@ -73,31 +77,31 @@ export default defineComponent({
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+ion-content{
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 1fr 2fr;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+#input_container ion-input{
+  height: 60px;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
+#input_container ion-button{
+  height: 40px;
+  width: 70px;
+}
 
-  color: #8c8c8c;
-
+ion-list{
+  width: 100vw;
+  padding: 0;
   margin: 0;
 }
 
-#container a {
-  text-decoration: none;
+#component{
+  display: grid;
+  grid-template-columns: 2fr 1fr;
 }
 </style>
